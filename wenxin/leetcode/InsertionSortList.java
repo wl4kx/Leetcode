a55096c406c63f43e1ab126bdb0cdaac37fd8724
Wenxin2013 <wenxin2013@gmail.com>
2015-08-21 00:32:32 -0400

    more problems

---
+++ wenxin/leetcode/InsertionSortList.java
@@ -0,0 +1,61 @@
+package wenxin.leetcode;
+
+/*
+ * Sort a linked list using insertion sort.
+ */
+public class InsertionSortList {
+
+	public static void main(String[] args) {
+		InsertionSortList instance = new InsertionSortList();
+		ListNode n1 = new ListNode(3);
+		ListNode n2 = new ListNode(2);
+		ListNode n3 = new ListNode(4);
+		n1.next = n2;
+		n2.next = n3;
+		
+		
+		
+		ListNode root =instance.insertionSortList(n1);
+		while(root!=null){
+			System.out.print(root.val+" -> ");
+			root = root.next;
+		}
+
+	}
+	
+    public ListNode insertionSortList(ListNode head) {
+        if(head==null || head.next==null){
+        	return head;
+        }
+        
+        ListNode start = new ListNode(0);
+        start.next = head;
+        ListNode p = head.next;
+        head.next = null;
+		while (p != null) {
+			ListNode temp = p.next;
+			ListNode p2 = start;
+			while (p2 != null) {
+				if(p2.next==null){
+					p2.next = p;
+					p.next = null;
+					break;
+				}else if(p.val<p2.next.val){
+					
+					p.next = p2.next;
+					p2.next = p;
+					break;
+				}else{
+					p2 = p2.next;
+				}
+			}
+
+			p=temp;
+
+		}
+		
+		return start.next;
+        
+    }
+
+}
---
+++ wenxin/leetcode/MaxPointsOnALine.java
@@ -0,0 +1,19 @@
+package wenxin.leetcode;
+
+import java.awt.Point;
+
+/*
+ * Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
+ */
+public class MaxPointsOnALine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+    public int maxPoints(Point[] points) {
+        
+    }
+    
+}
