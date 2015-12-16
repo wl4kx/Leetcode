5aa2302f41c029256ed661203477c04f8eb6bc1d
Wenxin <wl4kx@virginia.edu>
2015-12-15 22:06:06 -0500

    Additive Number

--- wenxin/leetcode/MainTest.java
+++ wenxin/leetcode/MainTest.java
@@ -3,6 +3,6 @@ package wenxin.leetcode;
 public class MainTest {
 	public static void main(String[] args) {
 		String s = "01234";
-		System.out.println(s.substring(0, 0));
+		System.out.println(s.substring(0, 1));
 	}
 }
