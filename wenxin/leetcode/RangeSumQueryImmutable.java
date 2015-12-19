21803848af02a6522f8642712d9078a3d86f6e37
Wenxin <wl4kx@virginia.edu>
2015-12-19 11:08:33 -0500

    Range Sum Query Immutable

--- wenxin/leetcode/RangeSumQueryImmutable.java
+++ wenxin/leetcode/RangeSumQueryImmutable.java
@@ -17,19 +17,25 @@ There are many calls to sumRange function.
 public class RangeSumQueryImmutable {
 	
 	public static void main(String[] args){
-		RangeSumQueryImmutable instance = new RangeSumQueryImmutable();
+		RangeSumQueryImmutable rsqi = new RangeSumQueryImmutable();
+		int nums[] = {-2, 0, 3, -5, 2, -1};
+		NumArray instance = rsqi.new NumArray(nums);
+		System.out.println(instance.sumRange(0,2));
+		System.out.println(instance.sumRange(2,5));
+		System.out.println(instance.sumRange(0,5));
 	}
-	
-	int[] sumArr;
 
-	public void NumArray(int[] nums) {
-        sumArr = new int[nums.length+1];
-        for (int i = 1; i < nums.length+1; i++) {
-			sumArr[i] = nums[i-1] + sumArr[i-1];
-		}
-    }
+    public class NumArray {
+    	int[] sumArr;
+        public NumArray(int[] nums) {
+            sumArr = new int[nums.length+1];
+            for (int i = 1; i < nums.length+1; i++) {
+    			sumArr[i] = nums[i-1] + sumArr[i-1];
+    		}
+        }
 
-    public int sumRange(int i, int j) {
-        return sumArr[j+1] - sumArr[i];
+        public int sumRange(int i, int j) {
+            return sumArr[j+1] - sumArr[i];
+        }
     }
 }
